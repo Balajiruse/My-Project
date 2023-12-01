77a35d2e2562759b602f4148e04149a9b1ad507c
Balajiruse <balaji.ruse@gmail.com>
2023-12-01 16:09:13 +0530

    first commit

---
+++ src/componetents/SideBar.jsx
@@ -0,0 +1,17 @@
+// eslint-disable-next-line react/prop-types
+export default function SideBar() {
+    return(
+    
+        <div className="side-bar">
+<div>
+    <ul className="actions">
+<li>one</li>
+<li>tew</li>
+<li>three</li>
+<li>foure</li>
+
+    </ul>
+</div>
+        </div>
+    );
+  }
